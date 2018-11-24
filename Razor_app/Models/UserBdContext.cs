43d0b9af6a77f7a66c1d76a242f443f375ec114a
Sakit <memmedlisakit@gmail.com>
2018-11-24 12:39:24 +0400

    post in razor pages

---
+++ Razor_app/Models/UserBdContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Razor_app.Models
+{
+    public class UserBdContext : DbContext
+    {
+        public UserBdContext(DbContextOptions dbContextOptions)
+            :base(dbContextOptions) { }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Razor_app/Pages/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Razor_app.Models;
+
+namespace Razor_app.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly UserBdContext _db;
+        public IndexModel(UserBdContext db)
+        {
+            _db = db;
+             Employee = new Employee();
+             Employees = new List<Employee>();
+        }
+
+
+        [BindProperty]
+        public Employee Employee { get; set; }
+
+        public List<Employee> Employees { get; set; }
+
+        public string[] Positions = new string[] { "Web Developer", "Desktop Developer", "Mobile Developer" };
+
+        public async Task OnGet()
+        {
+            this.Employees = await _db.Employees.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostCreateAsync()
+        { 
+            await _db.Employees.AddAsync(this.Employee);
+            await _db.SaveChangesAsync();
+            Employees = await _db.Employees.ToListAsync();
+            return Page();
+        }
+    }
+}
